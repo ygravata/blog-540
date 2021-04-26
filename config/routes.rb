72ddb9f7b793cd5d7768b4869367dd6ece208f4d
Yuri Gravata <ygravata@gmail.com>
2021-04-26 20:42:24 -0300

    adjusting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "articles#index"
-  resources :articles, except: %i[index]
+  resources :articles, except: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
